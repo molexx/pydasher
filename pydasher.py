33b9fc9f58296095e44852348200e9d26a3dbdcf
molexx <molexx@users.noreply.github.com>
2017-12-02 23:41:49 +0000

    add debug logging, switch on to find new button MAC addresses

--- pydasher.py
+++ pydasher.py
@@ -1,9 +1,14 @@
 import logging
+logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 import subprocess
 import yaml
 import os
+
+
+logging.info('starting...')
+
 
 # Get current working directory
 path = os.path.dirname(os.path.realpath(__file__))
@@ -34,6 +39,7 @@ def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].hwsrc in buttons.keys(): # Found a button's MAC
       dashlog.write("Found a button\n")
+      logging.info("Found a button - mac: " + pkt[ARP].hwsrc +  ", ip: " + pkt[ARP].psrc + " - sending event '" + buttons.get(pkt[ARP].hwsrc) + "'\n")
       # Fire a curl POST to HA's web API
       dashlog.write("\n")
       subprocess.call(["curl", "-H", "x-ha-access: " + password, "-X", "POST", "http://" + host + ":8123/api/events/" + buttons.get(pkt[ARP].hwsrc)], stdout=dashlog)
@@ -41,6 +47,9 @@ def arp_display(pkt):
     else:
       # Output unknown ARP's to the log as well
       dashlog.write("ARP Probe from unknown device: " + pkt[ARP].hwsrc + "\n")
+      logging.debug("ARP Probe from unknown device: mac: " + pkt[ARP].hwsrc + " ip: " +  pkt[ARP].psrc + "\n")
 
+
+logging.info('sniffing...')
 # Run it
 sniff(prn=arp_display, filter="arp", store=0, count=0)
